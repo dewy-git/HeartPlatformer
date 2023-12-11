9a140fa72be9c2717a8f0fbf8e1ca4569c196ab4
desk <dewu@s.sfusd.edu>
2023-12-11 15:27:21 -0800

    added powder bluee sky

---
+++ scripts/world.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+
+func _ready():
+	RenderingServer.set_default_clear_color(Color.POWDER_BLUE)
+	polygon_2d.polygon = collision_polygon_2d.polygon
+	
+
