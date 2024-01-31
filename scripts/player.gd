6bd26eeb7ec17e85b06eef6ca90b6bfdfef275bc
desk <dewu@s.sfusd.edu>
2024-01-31 13:13:41 -0800

    process 2 (coyote jump)

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,7 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -18,17 +18,21 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
