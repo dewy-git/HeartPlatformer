4e413d35ea64efb3bd9251058da370fa4df2a24c
desk <dewu@s.sfusd.edu>
2024-02-21 13:17:30 -0800

    progress 7 (auto/sloped tiles)

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.POWDER_BLUE)
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed():
